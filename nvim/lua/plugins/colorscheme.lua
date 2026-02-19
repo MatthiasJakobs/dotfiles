2b6fb8e4a2eab348d6588fe13995398c5bfd96a8
Matthias Jakobs <m.jakobs265@gmail.com>
2026-02-19 22:33:45 +0100

    changed to catppuccin

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+-- return {
+--     'folke/tokyonight.nvim',
+--     config = function()
+--         vim.cmd.colorscheme 'tokyonight'
+--     end
+-- }
+return {
+    'catppuccin/nvim',
+    config = function()
+        vim.cmd.colorscheme 'catppuccin'
+    end
+}
